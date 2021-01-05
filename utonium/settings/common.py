1c2c5d5875e2b421e518716f22e15ae58571e366
brunolcarli <brunolcarli@gmail.com>
2021-01-05 14:56:06 -0500

    ADD service version

--- utonium/settings/common.py
+++ utonium/settings/common.py
@@ -17,6 +17,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+VERSION = '0.0.0'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
