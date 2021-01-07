c990e945773372c7d4218415cc612eb05745ef1c
brunolcarli <brunolcarli@gmail.com>
2021-01-07 12:29:52 -0500

    CHANGE add cors

--- utonium/settings/common.py
+++ utonium/settings/common.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'powerpuff',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,20 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:8080',
+    'http://localhost:8222',
+]
+
 
 ROOT_URLCONF = 'utonium.urls'
 
